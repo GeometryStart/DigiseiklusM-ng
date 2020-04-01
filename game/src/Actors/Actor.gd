3806ef9c963582d18f278be1ecf9538f693f0016
Kerttu <kerttus@tlu.ee>
2020-04-01 19:10:43 +0300

    Added Character animation and change of scene

--- game/src/Actors/Actor.gd
+++ game/src/Actors/Actor.gd
@@ -4,8 +4,8 @@ class_name Actor
 
 const FLOOR_NORMAL: = Vector2.UP
 
-export var speed: = Vector2(400.0, 600.0)
-export var gravity: = 1000.0
+export var speed: = Vector2(400.0, 500.0)
+export var gravity: = 800.0
 
 var _velocity: = Vector2.ZERO
 
--- game/src/Actors/Player.gd
+++ game/src/Actors/Player.gd
@@ -1,25 +1,28 @@
 extends Actor
-var pos
-var screensize
-export var stomp_impulse: = 600.0
 
+export var stomp_impulse: = 1600.0
 
 func _on_StompDetector_area_entered(area: Area2D) -> void:
 	_velocity = calculate_stomp_velocity(_velocity, stomp_impulse)
 
-	screensize = get_viewport_rect().size
-
 func _physics_process(delta: float) -> void:
 	var is_jump_interrupted: = Input.is_action_just_released("jump") and _velocity.y < 0.0
 	var direction: = get_direction()
+	
 	_velocity = calculate_move_velocity(_velocity, direction, speed, is_jump_interrupted)
-	var snap: Vector2 = Vector2.DOWN * 40.0 if direction.y == 0.0 else Vector2.ZERO
+	var snap: Vector2 = Vector2.DOWN * 80.0 if direction.y == 0.0 else Vector2.ZERO
 	_velocity = move_and_slide_with_snap(
 		_velocity, snap, FLOOR_NORMAL, true
 	)
 
-
 func get_direction() -> Vector2:
+	if Input.is_action_pressed("move_right"):
+		$AnimatedSprite.play("run")
+	elif Input.is_action_pressed("jump"): 
+		$AnimatedSprite.play("jump")
+	else:
+		$AnimatedSprite.play("idle")
+	
 	return Vector2(
 		Input.get_action_strength("move_right") - Input.get_action_strength("move_left"),
 		-Input.get_action_strength("jump") if is_on_floor() and Input.is_action_just_pressed("jump") else 0.0
@@ -44,5 +47,6 @@ func calculate_move_velocity(
 func calculate_stomp_velocity(linear_velocity: Vector2, stomp_impulse: float) -> Vector2:
 	var stomp_jump: = -speed.y if Input.is_action_pressed("jump") else -stomp_impulse
 	return Vector2(linear_velocity.x, stomp_jump)
+
 
 
